7918f385769ced2c153633900141f6ef7c68f263
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2025-03-05 16:10:19 +0000

    change

--- src/app/enviroment/enviroment.component.ts
+++ src/app/enviroment/enviroment.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationError, Router } from '@angular/router';
+import { ResizeEvent } from 'angular-resizable-element';
 
 @Component({
   selector: 'app-enviroment',
@@ -7,6 +8,7 @@ import { NavigationError, Router } from '@angular/router';
   styleUrls: ['./enviroment.component.css']
 })
 export class EnviromentComponent {
+  public style: object = {};
 
   component = ''
 
@@ -22,4 +24,29 @@ export class EnviromentComponent {
   onClickMe() {
     console.log('...........')
   }
+
+
+  validate(event: ResizeEvent): boolean {
+    const MIN_DIMENSIONS_PX: number = 50;
+    if (
+      event.rectangle.width &&
+      event.rectangle.height &&
+      (event.rectangle.width < MIN_DIMENSIONS_PX ||
+        event.rectangle.height < MIN_DIMENSIONS_PX)
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  onResizeEnd(event: ResizeEvent): void {
+    this.style = {
+      position: 'fixed',
+      left: `${event.rectangle.left}px`,
+      top: `${event.rectangle.top}px`,
+      width: `${event.rectangle.width}px`,
+      height: `${event.rectangle.height}px`,
+    };
+  }
 }
+
--- src/app/enviroment/enviroment.module.ts
+++ src/app/enviroment/enviroment.module.ts
@@ -3,12 +3,16 @@ import { EnviromentComponent } from './enviroment.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { ResizableModule } from 'angular-resizable-element';
 
 @NgModule({
   declarations: [
     EnviromentComponent,
   ],
   imports: [
+    BrowserModule, 
+    ResizableModule,
     CommonModule,
     FormsModule,
     RouterModule,
